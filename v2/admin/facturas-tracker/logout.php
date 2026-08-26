3dabd5c85c264c1ffa8819f998a21cf14e37c179
Cristian <chivax@gmail.com>
2026-08-26 23:53:43 +0000

    Eliminar carpeta v2 duplicada

--- v2/admin/facturas-tracker/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-// El logout ahora es único y vive en la raíz del portal (cierra la sesión de todas las apps).
-require __DIR__ . '/../config.php';
-header('Location: ' . PORTAL_BASE . '/logout.php');
-exit;
--- v2/mdpstock/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'db.php';
-session_unset();
-session_destroy();
-header('Location: login.php');
-exit;
-?>
